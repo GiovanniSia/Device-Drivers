8b3615d6824328864a5a8b91979b052dc5f34aae
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-03-28 20:31:50 -0300

    add prints in miModulo.c

--- miModulo.c
+++ miModulo.c
@@ -3,11 +3,13 @@
 int init_module(void)
 { /* Constructor */
   printk(KERN_INFO "UNGS : Driver registrado\n");
+  printk(KERN_INFO "Hola Mundo\n");
   return 0;
 }
 void cleanup_module(void)
 { /* Destructor */
   printk(KERN_INFO "UNGS : Driver desregistrado\n");
+  printk(KERN_INFO "Chau Hola Mundo\n");
 }
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("UNGS");
