9bf85de07d30d1b30e7bbc6cb0c9f5d16e6c97ea
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-03-28 18:30:17 -0300

    Add character device kernel module

---
+++ chardevice.c
@@ -0,0 +1,124 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/device.h>
+
+#define DEVICE_NAME "chardev"
+#define BUF_LEN 80 
+
+static int major;
+static char msg[BUF_LEN + 1];
+
+static struct class *cls;
+
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
+
+static struct file_operations chardev_fops = {
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release,
+};
+
+int init_module(void) {
+    major = register_chrdev(0, DEVICE_NAME, &chardev_fops);
+    if (major < 0) {
+        pr_alert("Falló el registro del dispositivo de caracteres con el número mayor %d\n", major);
+        return major;
+    }
+    pr_info("Dispositivo registrado con el número mayor %d.\n", major);
+    
+    cls = class_create(THIS_MODULE, DEVICE_NAME);
+    device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
+
+    pr_info("Dispositivo creado en /dev/%s\n", DEVICE_NAME);
+
+    return 0;
+}
+
+void cleanup_module(void) {
+    device_destroy(cls, MKDEV(major, 0));
+    class_destroy(cls);
+    unregister_chrdev(major, DEVICE_NAME);
+
+    pr_info("Dispositivo desresgistrado\n");
+}
+
+static int device_open(struct inode *inode, struct file *file) {
+    try_module_get(THIS_MODULE);
+    return 0;
+}
+
+static int device_release(struct inode *inode, struct file *file) {
+    module_put(THIS_MODULE);
+    return 0;
+}
+
+static ssize_t device_read(struct file *filp, char __user *buffer, size_t length, loff_t *offset) {
+    int bytes_read = 0;
+    int msg_len = strlen(msg);
+    
+    if (*offset >= msg_len)
+        return 0;
+    
+    bytes_read = min(length, (size_t)(msg_len - *offset));
+
+    if (copy_to_user(buffer, msg + (msg_len - *offset - bytes_read), bytes_read))
+        return -EFAULT;
+
+    if (*offset + bytes_read == msg_len && (msg[msg_len - 1] != '\n' || *offset != 0)) {
+        if (copy_to_user(buffer + bytes_read, "\n", 1))
+            return -EFAULT;
+        bytes_read++;
+    }
+
+    *offset += bytes_read;
+
+    return bytes_read;
+}
+
+
+static ssize_t device_write(struct file *filp, const char __user *buff, size_t len, loff_t *off) {
+    int bytes_written = 0;
+    int start = 0;
+    int end = len - 1;
+    char temp;
+
+    if (len > BUF_LEN) {
+        pr_alert("El texto es demasiado largo.\n");
+        return -EINVAL;
+    }
+
+    if (copy_from_user(msg, buff, len)) {
+        pr_alert("Error al copiar desde el espacio de usuario.\n");
+        return -EFAULT;
+    }
+
+    pr_info("Texto recibido: %s\n", msg);
+
+
+    /* Evito invertir el carácter de salto de línea */
+    if (msg[end] == '\n') {
+        end--;
+    }
+
+    while (start < end) {
+        temp = msg[start];
+        msg[start] = msg[end];
+        msg[end] = temp;
+        start++;
+        end--;
+    }
+
+    bytes_written = len;
+    pr_info("Texto invertido: %s\n", msg);
+
+    return bytes_written;
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Giovanni Sia");
+MODULE_DESCRIPTION("Módulo del kernel de dispositivo de caracteres que permite la escritura de texto y al leer muestra el texto invertido");
